468379c52940e0edf6e9e4250996d7c776ee0fef
MARIUCHINAS <96793543+MARIUCHINAS@users.noreply.github.com>
2022-11-18 20:36:45 +0200

    Add files via upload

---
+++ Calc.py
@@ -0,0 +1,64 @@
+from time import sleep
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def modulus(x, y):
+    return x % y
+
+def power(x, y):
+    return x ** y
+
+def main():
+    print("Select operation.")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Modulus")
+    print("6. Power")
+
+    choice = input("Enter choice(1/2/3/4/5/6): ")
+
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    if choice == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
+        sleep(10)
+
+    elif choice == '2':
+        print(num1, "-", num2, "=", subtract(num1, num2))
+        sleep(10)
+
+    elif choice == '3':
+        print(num1, "*", num2, "=", multiply(num1, num2))
+        sleep(10)
+
+    elif choice == '4':
+        print(num1, "/", num2, "=", divide(num1, num2))
+        sleep(10)
+
+    elif choice == '5':
+        print(num1, "%", num2, "=", modulus(num1, num2))
+        sleep(10)
+
+    elif choice == '6':
+        print(num1, "**", num2, "=", power(num1, num2))
+        sleep(10)
+
+    else:
+        print("Invalid input")
+        sleep(10)
+
+if __name__ == '__main__':
+    main()
