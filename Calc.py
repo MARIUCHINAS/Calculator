a4ac467594181403899691e2f4c1acadba1dcfd7
MARIUCHINAS <96793543+MARIUCHINAS@users.noreply.github.com>
2022-12-01 14:20:38 +0200

    Add files via upload

--- Calc.py
+++ Calc.py
@@ -18,47 +18,48 @@ def modulus(x, y):
 def power(x, y):
     return x ** y
 
-def main():
-    print("Select operation.")
-    print("1. Add")
-    print("2. Subtract")
-    print("3. Multiply")
-    print("4. Divide")
-    print("5. Modulus")
-    print("6. Power")
+while True:
+ def main():
+     print("Select operation.")
+     print("1. Add")
+     print("2. Subtract")
+     print("3. Multiply")
+     print("4. Divide")
+     print("5. Modulus")
+     print("6. Power")
 
-    choice = input("Enter choice(1/2/3/4/5/6): ")
+     choice = input("Enter choice(1/2/3/4/5/6): ")
 
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
+     num1 = int(input("Enter first number: "))
+     num2 = int(input("Enter second number: "))
 
-    if choice == '1':
-        print(num1, "+", num2, "=", add(num1, num2))
-        sleep(10)
+     if choice == '1':
+         print(num1, "+", num2, "=", add(num1, num2))
+         sleep(1)
 
-    elif choice == '2':
-        print(num1, "-", num2, "=", subtract(num1, num2))
-        sleep(10)
+     elif choice == '2':
+         print(num1, "-", num2, "=", subtract(num1, num2))
+         sleep(1)
 
-    elif choice == '3':
-        print(num1, "*", num2, "=", multiply(num1, num2))
-        sleep(10)
+     elif choice == '3':
+         print(num1, "*", num2, "=", multiply(num1, num2))
+         sleep(1)
 
-    elif choice == '4':
-        print(num1, "/", num2, "=", divide(num1, num2))
-        sleep(10)
+     elif choice == '4':
+         print(num1, "/", num2, "=", divide(num1, num2))
+         sleep(1)
 
-    elif choice == '5':
-        print(num1, "%", num2, "=", modulus(num1, num2))
-        sleep(10)
+     elif choice == '5':
+         print(num1, "%", num2, "=", modulus(num1, num2))
+         sleep(1)
 
-    elif choice == '6':
-        print(num1, "**", num2, "=", power(num1, num2))
-        sleep(10)
+     elif choice == '6':
+         print(num1, "**", num2, "=", power(num1, num2))
+         sleep(1)
 
-    else:
-        print("Invalid input")
-        sleep(10)
+     else:
+         print("Invalid input")
+         sleep(1)
 
-if __name__ == '__main__':
-    main()
+ if __name__ == '__main__':
+     main()
